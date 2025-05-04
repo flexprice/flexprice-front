d775fa9346b9b94fdd0d268320c4477d492d58d8
omkar sonawane <omkarsonawane622@gmail.com>
2025-05-04 13:04:41 +0530

    refactor(query-builder): improve styling and structure of filter and sort components

--- src/components/atoms/Toggle/Toggle.tsx
+++ src/components/atoms/Toggle/Toggle.tsx
@@ -2,6 +2,7 @@ import { Label } from '@/components/ui/label';
 import { FormHeader, Spacer } from '..';
 import { Switch } from '@/components/ui/switch';
 import { FC } from 'react';
+import { cn } from '@/lib/utils';
 interface Props {
 	onChange: (value: boolean) => void;
 	checked: boolean;
@@ -9,14 +10,15 @@ interface Props {
 	label?: string;
 	description?: string;
 	error?: string;
+	className?: string;
 }
 
-const Toggle: FC<Props> = ({ onChange, checked, description, error, label, title }) => {
+const Toggle: FC<Props> = ({ onChange, checked, description, error, label, title, className }) => {
 	return (
 		<div>
 			<FormHeader title={title} variant='form-component-title' />
 			<div className='flex items-start space-x-4 font-open-sans'>
-				<Switch id='airplane-mode' checked={checked} onCheckedChange={onChange} />
+				<Switch id='airplane-mode' checked={checked} onCheckedChange={onChange} className={cn(className, 'h-7 text-xs')} />
 				<Label htmlFor='airplane-mode'>
 					<p className='font-medium text-sm text-[#18181B] peer-checked:text-black'>{label}</p>
 					<Spacer height={'4px'} />
